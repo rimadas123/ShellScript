4b07be8a234411c8ec999e465984ef11eab63507
rimadas123 <rdas5969@gmail.com>
2020-03-05 10:35:42 +0530

    Added : Compute factors of number N using prime factorization

---
+++ repetition_programs/factorizationMethod.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -x
+
+echo -n "Enter a number"
+read number
+
+for (( i=2; i<=$number; i++ ))
+do
+	while [ $(( $number%$i )) == 0 ] 
+	do
+		echo $i
+		number=$(( $number/$i ))
+	done
+done
+
+
